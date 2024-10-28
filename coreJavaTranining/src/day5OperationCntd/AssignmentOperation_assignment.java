9f82063991d67c53d51ff2fc7f9be5134947aac7
bettyabebe2009 <bettyabebe2009@gmail.com>
2024-10-28 11:49:09 -0500

    day3 to day6
    day3 to day6

---
+++ coreJavaTranining/src/day5OperationCntd/AssignmentOperation_assignment.java
@@ -0,0 +1,12 @@
+package day5OperationCntd;
+
+public class AssignmentOperation_assignment {
+    public static void main(String[] args) {
+        // assignment check the number is even or odd
+        int a = 17;
+        String result = (a % 2 == 0)? a + " is even" : a + " is odd";
+        System.out.println( result );
+
+
+    }
+}
---
+++ coreJavaTranining/src/day6ConditionalStatments/IfElseCondition.java
@@ -0,0 +1,30 @@
+package day6ConditionalStatments;
+
+public class IfElseCondition {
+    // Here are some real-world examples where an if else condition is used in Java:
+    // There is a single condition to be checked but either of job to be done
+    // 1. User Login Authentication - A typical use case in applications where the user is trying to log in,
+    // and the system checks if the credentials are correct.
+    // 2. Bank ATM Transaction - When a user tries to withdraw money from an ATM, the system checks
+    // if the balance is sufficient.
+    // 3. Age Verification for Voting - A program that checks if a user is eligible to vote based on their age.
+    // 4. Hotel Room Reservation - A program that checks room availability and books a room accordingly.
+    // 5. Checking if a number is even or odd
+    // 6. Largest of the two number
+    public static void main(String[] args) {
+
+
+        String username = "test";
+        String password = "test123";
+
+        String correctUsername = "test";
+        String correctPassword = "test123";
+
+        if(username.equals(correctUsername) && password.equals(correctPassword)) {
+            System.out.println("Login Successful");
+
+        } else {
+            System.out.println("Invalid credentialds, please try again.");
+        }
+    }
+}
